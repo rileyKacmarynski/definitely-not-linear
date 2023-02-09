ece0ac50371ec1c90bd2dadadde9ce93dcddb3a5
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-02-08 20:09:46 -0600

    main hero is coming together

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,25 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+      screens: {
+        '2xl': '1440px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-	],
-}
+    require('tailwindcss-animate'),
+  ],
+};
