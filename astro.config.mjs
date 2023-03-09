89dbd82a9fd19b0e078abcb6f6dc0d0644ac451c
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-03-09 12:51:59 -0600

    finish up image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), image()]
 });
